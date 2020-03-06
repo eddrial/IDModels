e80bb6467421554549134f7e45f038c470bd7d2f
Ed Rial <eddrial@hotmail.com>
2020-03-06 09:44:16 +0100

    Multiple bodies, one container...

--- ATHENA_II/APPLE2p5/ATHENA_Model.py
+++ ATHENA_II/APPLE2p5/ATHENA_Model.py
@@ -4,13 +4,28 @@ Created on 3 Mar 2020
 @author: oqb
 '''
 from wRadia import wradObj as wrd
+import radia as rd
 import numpy as np
 
-def parameters():
-    pass
+class model_parameters():
+    
+    def __init__(self):
+        self.origin = np.zeros(3)
+        self.mainmagthick = 5
+        self.mainmagdimension = 30
+        self.clampcut = 5
+        self.direction = 'y'
+    
 
-def appleMagnet():
-    pass
+
+def appleMagnet(parameter_class, mag_center):
+    a = wrd.wradObjCnt([])
+    p1 = wrd.wradObjThckPgn(0, parameter_class.mainmagthick, [[-5,-5],[-5,5],[5,5],[5,-5]], parameter_class.direction)
+    p2 = wrd.wradObjThckPgn(10, parameter_class.mainmagthick, [[-5,-5],[-5,5],[5,5],[5,-5]], parameter_class.direction)
+    p3 = wrd.wradObjThckPgn(20, parameter_class.mainmagthick, [[-5,-5],[-5,5],[5,5],[5,-5]], parameter_class.direction)
+    
+    a = wrd.wradObjAddToCnt(a.radobj, [p1.radobj,p2.radobj,p3.radobj])
+    return a
 
 def appleArray():
     pass
@@ -21,19 +36,27 @@ def appleTotal():
 if __name__ == '__main__':
     
     #my parameter list
-    origin = np.zeros(3)
+    '''    origin = np.zeros(3)
     mainmagthick = 5
     mainmagdimension = 30
     clamput = 5
     direction = 'y'
+    '''
+    #ATHENA_II Parameters
+    AII = model_parameters()
     
     #my magnet model
-    basemagnet = wrd.wradObjThckPgn(0, mainmagthick, [[-5,-5],[-5,5],[5,5],[5,-5]], direction)
+    basemagnet = wrd.wradObjThckPgn(0, AII.mainmagthick, [[-5,-5],[-5,5],[5,5],[5,-5]], AII.direction)
     
+    a = appleMagnet(AII,4)
     #my beam model
     
     #my apple model
     print(basemagnet.radobj)
-    print(origin)
+    print(AII.origin)
+    print(a.objectlist)
     
+    rd.ObjDrwOpenGL(a.radobj)
+    
+    print(a.objectlist)
     pass
