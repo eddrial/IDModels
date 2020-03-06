8de8c9943b7716d16ff07d66f0043d11d4ef03e5
Ed Rial <eddrial@hotmail.com>
2020-03-06 15:55:36 +0100

    Multiple magnets in row

--- ATHENA_II/APPLE2p5/ATHENA_Model.py
+++ ATHENA_II/APPLE2p5/ATHENA_Model.py
@@ -22,15 +22,28 @@ import numpy as np
 class model_parameters():
     
     def __init__(self):
+        #general
         self.origin = np.zeros(3)
-        self.mainmagthick = 5
+
+        
+        #Undulator
+        self.applePeriods = 10;
+        self.appleMagnets = self.applePeriods*4 + 1;
+        self.minimumgap = 2
+        self.rowtorowgap = 0.5
+        self.shim = 0.05
+        self.periodlength = 15
+        
+        #magnet shape
+        self.mainmagthick = (self.periodlength-4 * self.shim) / 4.0
         self.mainmagdimension = 30
         self.clampcut = 5
         self.direction = 'y'
         
+        #magnetmaterial
         self.ksi = [.019, .06]
-        
-        self.magnet_material = wradMat.wradMatLin([1,0],[0,1,0])
+        self.M = 1.21*1.344
+        self.magnet_material = wradMat.wradMatLin(self.ksi,self.M)
         
         #wrd.wradObj
     
@@ -61,8 +74,18 @@ def appleMagnet(parameter_class, mag_center, magnet_material, loc_offset = [0,0,
     
     return a
 
-def appleArray():
-    pass
+def appleArray(parameter_class,loc_offset, magnet_material):
+    a = wrd.wradObjCnt([])
+    for x in range(0,parameter_class.appleMagnets):
+        mag = appleMagnet(parameter_class, loc_offset[1], magnet_material, loc_offset) 
+        loc_offset[1] += 10
+        a.wradObjAddToCnt([mag])
+        
+    return a
+        
+    #mag = appleMagnet(AII,4,materiald,[z,y,x])
+    #mag apply magnetisation and colour
+    #add to container
 
 def appleTotal():
     pass
@@ -80,19 +103,28 @@ if __name__ == '__main__':
     AII = model_parameters()
     
     #magnet Material
-    mat1 = wradMat.wradMatLin(AII.ksi,[0,1,0])
+    mat1 = wradMat.wradMatLin(AII.ksi,[0,AII.M,0])
     
     #my magnet model
     
     a = appleMagnet(AII,4,mat1,[10,20,30])
+    magcol = [x / 2.0 for x in [0,AII.M,0]]
+    a.wradObjDrwAtr([x / 2.0 for x in [1,AII.M,0]], 2)
+    a.wradObjDivMag([3,2,1])
     
     #my beam model
+    
+    b = appleArray(AII, [-AII.mainmagdimension/2.0 - AII.minimumgap,0,-AII.mainmagdimension/2.0 - AII.rowtorowgap], mat1)
     
     #my apple model
     print(AII.origin)
     print(a.objectlist)
     
     rd.ObjDrwOpenGL(a.radobj)
+    rd.ObjDrwOpenGL(b.radobj)
+    
+    
+    
     input("Press Enter to continue...")
     
     # All examples built from
