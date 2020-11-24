95770e21fa54191e9731ad628558424ecb99ff2e
Ed Rial <eddrial@hotmail.com>
2020-11-24 09:01:49 +0100

    Creating package for analysis of wrad containers, and to read, plot, write results

---
+++ idanalysis/analysis_functions.py
@@ -0,0 +1,17 @@
+'''
+Created on 24 Nov 2020
+
+@author: oqb
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
