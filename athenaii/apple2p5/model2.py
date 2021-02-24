b82f44f13378412fd27e4fe0c94c8ddba786e5f8
Ed Rial <eddrial@hotmail.com>
2021-02-24 17:32:57 +0100

    CompAPPLE2 tidied, APPLE in same format

--- athenaii/apple2p5/model2.py
+++ athenaii/apple2p5/model2.py
@@ -36,6 +36,8 @@ from idcomponents import parameters
 from idcomponents import magnet_shapes as ms
 from idcomponents import halbach_arrays as ha
 
+import matplotlib.gridspec as gridspec
+
 class plainAPPLE():
     '''
     classdocs
@@ -396,7 +398,125 @@ class compensatedAPPLEv2():
         print('my compensated APPLE calculated at a gap of {}mm'.format(mp.gap))
         '''
         Constructor
-        '''
+        
+    '''
+    #def plot_fields(self):
+        #plot stream fields in XZ with block outlines
+        #plot magnetisation vectors in XZ with block outline
+        #plot stream fields in XS with block outlines
+        #plot magnetisation vectors in XS with block outlines
+        
+        #plot on axis field with block outlines
+        #plot on field peak field with block outlines.
+        
+        #plot contour plot XS
+        
+    def BfieldStreamPlot(self,fields = 'bxbz', plotdims = np.array([0,100,0,100])):
+        
+        #region of V magnets
+        Zv, Xv = np.mgrid[plotdims[2]:plotdims[3]:41j, plotdims[0]:plotdims[1]:41j]
+        Bxv = Xv.copy()
+        Bzv = Zv.copy()
+        
+        for i in range(len(Xv)):
+            for j in range(len(Zv)):
+                #print ('coords are {}'.format([Xv[i,j],Zv[i,j]]))
+                Bxv[i,j],Bzv[i,j] = rd.Fld(self.cont.radobj,fields,[Xv[i,j],0,Zv[i,j]]) 
+                #print ('the field at those coords are Bx: {} Bz: {}'.format(Bxv[i,j],Bzv[i,j]))
+        
+        fig = plt.figure(figsize=(7, 9))
+        gs = gridspec.GridSpec(nrows=3, ncols=2, height_ratios=[1, 1, 2])
+        
+        #  Varying density along a streamline
+        ax0 = fig.add_subplot(gs[0, 0])
+        ax0.streamplot(Xv, Zv, Bxv, Bzv, density=[0.5, 1])
+        for i in range(2):
+            for j in range(4,7,2):
+                ax0.plot(self.cont.objectlist[j].objectlist[0].objectlist[0].objectlist[i].vertices[:,0],
+                         self.cont.objectlist[j].objectlist[0].objectlist[0].objectlist[i].vertices[:,2], color = 'k')
+        ax0.set_title('Vertical Comp Magnets')
+        
+        ax0.set_aspect('equal')
+        
+        return ax0
+        # Varying color along a streamline
+#        ax1 = fig.add_subplot(gs[0, 1])
+#        strm = ax1.streamplot(Xv, Zv, Bxv, Bzv, color=Bzv, linewidth=2, cmap='autumn')
+#        fig.colorbar(strm.lines)
+#        for i in range(2):
+#            for j in range(4,7,2):
+#                ax1.plot(self.objectlist[j].objectlist[0].objectlist[0].objectlist[i].vertices[:,0],
+#                         self.objectlist[j].objectlist[0].objectlist[0].objectlist[i].vertices[:,2], color = 'k')
+#        ax1.set_title('Vertical Comp Magnets')
+        
+        #region of H magnets
+ #       Zh, Xh = np.mgrid[-10:10:41j, 20:40:41j]
+ #       BXh = Xh.copy()
+ #       BZh = Zh.copy()
+        
+#        for i in range(len(Xh)):
+#            for j in range(len(Zh)):
+                #print ('coords are {}'.format([X[i,j],Y[i,j]]))
+#                BXh[i,j],BZh[i,j] = rd.Fld(self.radobj,'bxbz',[Xh[i,j],0,Zh[i,j]]) 
+                #print ('the field at those coords are Bx: {} Bz: {}'.format(a,b))
+#        ax1.set_aspect('equal')
+        
+        #  Varying density along a streamline
+#        ax2 = fig.add_subplot(gs[1, 0])
+#        ax2.streamplot(Xh, Zh, BXh, BZh, density=[0.5, 1])
+#        ax2.set_title('Horizontal Comp Magnets')
+        
+#        for i in range(2):
+#            for j in range(7,12,4):
+#                ax2.plot(self.objectlist[j].objectlist[0].objectlist[0].objectlist[i].vertices[:,0],
+#                         self.objectlist[j].objectlist[0].objectlist[0].objectlist[i].vertices[:,2], color = 'k')
+#        ax2.set_aspect('equal')
+        
+        # Varying color along a streamline
+#        ax3 = fig.add_subplot(gs[1, 1])
+#        strm = ax3.streamplot(Xh, Zh, BXh, BZh, color=BZh, linewidth=2, cmap='autumn')
+#        fig.colorbar(strm.lines)
+#        for i in range(2):
+#            for j in range(7,12,4):
+#                ax3.plot(self.objectlist[j].objectlist[0].objectlist[0].objectlist[i].vertices[:,0],
+#                         self.objectlist[j].objectlist[0].objectlist[0].objectlist[i].vertices[:,2], color = 'k')
+#        ax3.set_title('Horizontal Comp Magnets')
+#        ax3.set_aspect('equal')
+        
+        #region of Functional magnets
+#        Zf, Xf = np.mgrid[-20:20:41j, -20:20:41j]
+#        BXf = Xf.copy()
+#        BZf = Zf.copy()
+        
+#        for i in range(len(Xf)):
+#            for j in range(len(Zf)):
+                #print ('coords are {}'.format([X[i,j],Y[i,j]]))
+#                BXf[i,j],BZf[i,j] = rd.Fld(self.radobj,'bxbz',[Xf[i,j],0,Zf[i,j]]) 
+                #print ('the field at those coords are Bx: {} Bz: {}'.format(a,b))
+        
+        
+        #  Varying density along a streamline
+##        ax4 = fig.add_subplot(gs[2, 0])
+#        ax4.streamplot(Xf, Zf, BXf, BZf, density=[0.5, 1])
+#        ax4.set_title('Functional Magnets')
+#        for i in range(3):
+#            for j in range(4):
+#                ax4.plot(self.objectlist[j].objectlist[0].objectlist[0].objectlist[i].vertices[:,0],
+#                         self.objectlist[j].objectlist[0].objectlist[0].objectlist[i].vertices[:,2], color = 'k')
+#        ax4.set_aspect('equal')
+        
+        # Varying color along a streamline
+#        ax5 = fig.add_subplot(gs[2, 1])
+#        strm = ax5.streamplot(Xf, Zf, BXf, BZf, color=BZf, linewidth=2, cmap='autumn')
+#        fig.colorbar(strm.lines)
+#        ax5.set_title('Functional Magnets')
+#        for i in range(3):
+#            for j in range(4):
+#                ax5.plot(self.objectlist[j].objectlist[0].objectlist[0].objectlist[i].vertices[:,0],
+#                         self.objectlist[j].objectlist[0].objectlist[0].objectlist[i].vertices[:,2], color = 'k')
+#        ax5.set_aspect('equal')
+        
+#        pass
 
 
 if __name__ == '__main__':
