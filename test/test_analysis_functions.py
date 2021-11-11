2fe83449f41c51173ac27ace86caa47deb5da42b
Ed Rial <eddrial@hotmail.com>
2021-11-11 14:15:43 +0100

    Initiate test for Analysis Function

---
+++ test/test_analysis_functions.py
@@ -0,0 +1,18 @@
+'''
+Created on 11 Nov 2021
+
+@author: oqb
+'''
+import unittest
+
+
+class Test(unittest.TestCase):
+
+
+    def testCaseFullSolution(self):
+        pass
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testCaseFullSolution']
+    unittest.main()
