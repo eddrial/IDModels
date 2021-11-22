8a7ed1b1975d0c0f41d6fee17219822bf9c1b891
Ed Rial <eddrial@hotmail.com>
2021-11-22 15:45:44 +0100

    modify setup & travis put docker back to wradia

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
       version = "0.1",
       description='Python Modelling and Analysis of Undulators, using Radia, and wrapped Radia',
       dependency_links=['http://github.com/ochubar/Radia/tarball/master#egg=package-1.0&subdirectory=env/radia_python',
-                        'http://github.com/eddrial/wradia/tree/master/wradia/tarball/master#egg=package-0.1'],
+                        'http://github.com/eddrial/wradia/tarball/master#egg=package-1.0'],
       packages = ['idanalysis']#,
 #      install_requires = ['some-pkg @ git+https://github.com/eddrial/wRadia@master']#,
 #                          'radia',
