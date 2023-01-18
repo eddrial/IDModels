df4da9763e650f15eca5bc40dc6b7067dcf46770
Ed Rial <eddrial@hotmail.com>
2023-01-18 13:45:41 +0100

    another working file

--- athenaii_studies/studies20220509.py
+++ athenaii_studies/studies20220509.py
@@ -32,27 +32,28 @@ from wradia.wrad_obj import wradObjCnt
 
 if __name__ == '__main__':
         ### developing Case Solution ###
-    lam_u = 18
+    lam_u = 15
     
-    test_hyper_params = parameters.model_parameters(Mova = 20, 
+    test_hyper_params = parameters.model_parameters(Mova = 45, 
                                              periods = 4, 
                                              periodlength = lam_u,
                                              nominal_fmagnet_dimensions = [15.0,0.0,15.0], 
                                              #nominal_cmagnet_dimensions = [10.0,0.0,15.0],
                                              nominal_vcmagnet_dimensions = [7.5,0.0,15.0],
                                              nominal_hcmagnet_dimensions = [7.5,0.0,15.0], 
-                                             compappleseparation = 7.5,
+                                             compappleseparation = 0,
                                              apple_clampcut = 3.0,
                                              comp_magnet_chamfer = [3.0,0.0,3.0],
                                              magnets_per_period = 6,
-                                             gap = 6.2, 
-                                             rowshift = lam_u*21.1/64.0,
+                                             gap = 0.5, 
+                                             rowshift = 0,#lam_u*21.55/64.0,
                                              shiftmode = 'circular',
                                              block_subdivision = [1,1,1]
                                              )
     #a = id.compensatedAPPLEv2(test_hyper_params)
     #a = id.plainAPPLE(test_hyper_params, fmagnet=ms.appleMagnetFELr4)
-    a = id.plainAPPLE(test_hyper_params, fmagnet=ms.appleMagnet)
+    a = id.plainAPPLE(test_hyper_params, fmagnet=ms.appleMagnetvarProfile)
+    #a = id.plainAPPLE(test_hyper_params, fmagnet=ms.appleMagnet)
 #    
 
         #add stayclear
@@ -76,14 +77,14 @@ if __name__ == '__main__':
     
     ### Developing Model Solution ### Range of gap. rowshift and shiftmode ###
     gaprange = np.array([2.2,5,10,30])
-    shiftrange = np.arange(-7.5,7.51, 1.875)
+    shiftrange = np.arange(0,7.51, 7.5)
     shiftmoderange = ['linear']
     
     #scan_parameters = parameters.scan_parameters(periodlength = test_hyper_params.periodlength, gaprange = gaprange, shiftrange = shiftrange, shiftmoderange = shiftmoderange)
     scan_parameters = parameters.scan_parameters(periodlength = test_hyper_params.periodlength, gaprange = gaprange, shiftrange = shiftrange, shiftmoderange = shiftmoderange)
     
-    sol1 = af.Solution(test_hyper_params, scan_parameters,property = ['B','Forces'])
-    sol1.solve(property = ['B','Forces'])
+    sol1 = af.Solution(test_hyper_params, scan_parameters,property = ['B'])
+    sol1.solve(property = ['B'])
     
     timestamp = datetime.now()
     rootname = 'dev_{}'.format(timestamp.strftime("%Y%m%d_%H%M%S"))
