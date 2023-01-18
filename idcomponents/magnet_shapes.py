08caeeaeaa9e0fc21365bd4c27d47f4996510572
Ed Rial <eddrial@hotmail.com>
2023-01-18 13:44:12 +0100

    variable APPLE magnet profile class added

--- idcomponents/magnet_shapes.py
+++ idcomponents/magnet_shapes.py
@@ -296,6 +296,68 @@ class appleMagnetFELr6():
         self.cont.wradObjDivMag(mp.block_subdivision)
         self.cont.wradObjDrwAtr(colour = 'default', linethickness = 2)
 
+class appleMagnetvarProfile():
+    '''
+    classdocs
+    '''
+    def __init__(self, 
+                 model_parameters = parameters.model_parameters(), 
+                 magnet_centre  = [0,0,0], 
+                 this_magnet_material = 'default', 
+                 colour = 'default', 
+                 magnet_thickness = 'default'):
+        
+        '''
+        Constructor
+        '''
+        
+        mp = model_parameters
+        #to be put into model parameters
+        profile = mp.magnet_profile * mp.apple_clampcut
+        prof_point = np.linspace(0,mp.nominal_fmagnet_dimensions[0]-mp.apple_clampcut,len(profile))
+        
+        
+        self.magnet_centre = magnet_centre
+        
+        if this_magnet_material == 'default':
+            this_magnet_material = mp.magnet_material
+            
+        if magnet_thickness == 'default':
+            magnet_thickness = mp.nominal_fmagnet_dimensions[1]
+        
+        '''orientation order z,y,x'''
+        self.cont = wrd.wradObjCnt([])  # Container
+    #    a.magnet_material = magnet_material
+    #p1 is the min body of the magnet
+        p1 = wrd.wradObjThckPgn(magnet_centre[1], magnet_thickness, [[magnet_centre[0]-mp.nominal_fmagnet_dimensions[0]/2 + mp.apple_clampcut,magnet_centre[2]-mp.nominal_fmagnet_dimensions[2]/2],
+                                                                  [magnet_centre[0]-mp.nominal_fmagnet_dimensions[0]/2 + mp.apple_clampcut,magnet_centre[2]+mp.nominal_fmagnet_dimensions[2]/2],
+                                                                  [magnet_centre[0]+mp.nominal_fmagnet_dimensions[0]/2 - mp.apple_clampcut,magnet_centre[2]+mp.nominal_fmagnet_dimensions[2]/2],
+                                                                  [magnet_centre[0]+mp.nominal_fmagnet_dimensions[0]/2 - mp.apple_clampcut,magnet_centre[2]-mp.nominal_fmagnet_dimensions[2]/2]], 
+                                                                  mp.direction)
+    #p2 is the bottom part
+        p2 = wrd.wradObjThckPgn(magnet_centre[1], magnet_thickness, [[magnet_centre[0]-mp.nominal_fmagnet_dimensions[0]/2,magnet_centre[2]-mp.nominal_fmagnet_dimensions[2]/2],
+                                                                  [magnet_centre[0]-mp.nominal_fmagnet_dimensions[0]/2,magnet_centre[2]+mp.nominal_fmagnet_dimensions[2]/2 - mp.apple_clampcut],
+                                                                  [magnet_centre[0]-mp.nominal_fmagnet_dimensions[0]/2 + mp.apple_clampcut,magnet_centre[2]+mp.nominal_fmagnet_dimensions[2]/2 - mp.apple_clampcut],
+                                                                  [magnet_centre[0]-mp.nominal_fmagnet_dimensions[0]/2 + mp.apple_clampcut,magnet_centre[2]-mp.nominal_fmagnet_dimensions[2]/2]], 
+                                                                  mp.direction)
+        
+        self.cont.wradObjAddToCnt([p1,p2])
+    #pn is the trapezia segments of profile top
+    
+        for i in range(len(profile)-1):
+            pn = wrd.wradObjThckPgn(magnet_centre[1], magnet_thickness, [[magnet_centre[0]+mp.nominal_fmagnet_dimensions[0]/2 - mp.apple_clampcut+profile[i],magnet_centre[2]-mp.nominal_fmagnet_dimensions[2]/2 + mp.apple_clampcut+prof_point[i]],
+                                                                  [magnet_centre[0]+mp.nominal_fmagnet_dimensions[0]/2 - mp.apple_clampcut+profile[i+1],magnet_centre[2]-mp.nominal_fmagnet_dimensions[2]/2+mp.apple_clampcut+prof_point[i+1]],#b
+                                                                  [magnet_centre[0]+mp.nominal_fmagnet_dimensions[0]/2 - mp.apple_clampcut,magnet_centre[2]-mp.nominal_fmagnet_dimensions[2]/2+mp.apple_clampcut+prof_point[i+1]],
+                                                                  [magnet_centre[0]+mp.nominal_fmagnet_dimensions[0]/2 - mp.apple_clampcut,magnet_centre[2]-mp.nominal_fmagnet_dimensions[2]/2+mp.apple_clampcut+prof_point[i]]], 
+                                                                  mp.direction)
+        
+            self.cont.wradObjAddToCnt([pn])
+        
+        self.cont.wradMatAppl(this_magnet_material)
+        self.cont.wradObjDivMag(mp.block_subdivision)
+        self.cont.wradObjDrwAtr(colour = 'default', linethickness = 2)
+
+
 class tribsAppleMiddleMagnet():
     '''
     classdocs
@@ -347,7 +409,7 @@ class tribsAppleMiddleMagnet():
     
     
 if __name__ == '__main__':
-    a = tribsAppleMiddleMagnet()
+    a = appleMagnetvarProfile()
     b = compMagnet()
     
     rd.ObjDrwOpenGL(a.cont.radobj)
